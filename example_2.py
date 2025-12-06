3ef0574dee2ce4ce492d500a37ce2c389a6ca6f0
Imesh Rathnayake <imeshrathnayake@hotmail.com>
2025-12-06 22:34:04 +0530

    Edit the code in Example_2.py

--- example_2.py
+++ example_2.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsRegressor
 
+
 # Download and prepare the data
 data_root = "https://github.com/ageron/data/raw/main/"
 lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
